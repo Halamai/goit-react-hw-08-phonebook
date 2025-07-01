f5f59f142f1956a8026ea423e25cbd8c7a0976fd
pavlo <pavlo>
2025-07-01 17:04:26 +0300

    add-change

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -1,20 +1,10 @@
 window.CMS_MANUAL_INIT = true;
 
-function loadCMS() {
+document.addEventListener("DOMContentLoaded", () => {
   const script = document.createElement("script");
   script.src = "https://unpkg.com/netlify-cms@^2.10.192/dist/netlify-cms.js";
   script.onload = () => {
-    // Коли CMS завантажено і елемент існує — ініціалізуємо
-    const checkReady = () => {
-      if (window.CMS && document.getElementById("nc-root")) {
-        window.CMS.init({ config: "/admin/config.yml" });
-      } else {
-        setTimeout(checkReady, 50);
-      }
-    };
-    checkReady();
+    CMS.init({ config: "/admin/config.yml" });
   };
-  document.head.appendChild(script);
-}
-
-loadCMS();
+  document.body.appendChild(script);
+});
